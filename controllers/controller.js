73d2b7666d8f1b237eae566c9370ac97f08027fb
galihsatriaadhiputra <galihsadhira@gmail.com>
2022-06-19 23:30:36 +0700

    Initiate

---
+++ controllers/controller.js
@@ -0,0 +1,131 @@
+const { Product, Category, Ingredient, Sale } = require("../models/index");
+
+class Controller {
+  async addProduct(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async getAllProducts(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async getOneProduct(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async editProduct(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async deleteProduct(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async addCategory(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async getAllCategories(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async getOneCategory(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async editCategory(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async deleteCategory(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async addIngredient(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async getAllIngredients(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async getOneIngredient(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async editIngredient(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async deleteIngredient(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async addSale(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async getAllSales(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async getOneSale(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async editSale(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+  async deleteSale(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = {
+  Controller
+};
---
+++ routes/index.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const { Controller } = require("../controllers/controller");
+const router = require("express").Router();
+
+//add new product
+router.post("/products");
+
+//get all product
+router.get("/products");
+
+//get one product
+router.get("/products/:id");
+
+//edit one product
+router.put("/products/:id");
+
+//edit one product
+router.delete("/products/:id");
+
+//=============================
+
+//add new categories
+router.post("/categories");
+
+//get all categories
+router.get("/categories");
+
+//get one category
+router.get("/categories/:id");
+
+//edit category
+router.put("/categories/:id");
+
+//delete category
+router.delete("/categories/:id");
+
+//=============================
+
+//add new ingredient
+router.post("/ingredients");
+
+//get all ingredients
+router.get("/ingredients");
+
+//get one ingredient
+router.get("/ingredients/:id");
+
+//edit ingredient
+router.put("/ingredients/:id");
+
+//delete ingredient
+router.delete("/ingredients/:id");
+
+//=============================
+
+//add new sales data
+router.post("/sales");
+
+//get all sales
+router.get("/sales");
+
+//get one sale
+router.get("/sales/:id"); //by date?
+
+//edit sales
+router.put("/sales/:id"); //authorization
+
+//delete sales
+router.delete("/sales/:id");
+
+router.module.exports = router;
