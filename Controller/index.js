fe54d8fd01dd28e7e38fa5290bb177700582039a
harsenn24 <caesarharsenn@gmail.com>
2022-06-21 16:49:59 +0700

    finish register

--- Controller/index.js
+++ Controller/index.js
@@ -7,7 +7,41 @@ const midtransClient = require("midtrans-client");
 const { sendEmail } = require("../helps/sendemail");
 
 class Controller {
-  
+  static async register(req, res, next) {
+    try {
+      const { email, password, name } = req.body;
+      const inputData = { email, password, name };
+
+      if (!email) {
+        throw { name: "Email is required" };
+      }
+
+      if (!password) {
+        throw { name: "Password is required" };
+      }
+
+      if (!name) {
+        throw { name: "Name is required" };
+      }
+
+      const newUser = await User.create(inputData);
+
+      sendEmail(email, name, null);
+
+      const result = {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+      };
+
+      res.status(201).json(result);
+
+      
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
 }
 
 module.exports = {
