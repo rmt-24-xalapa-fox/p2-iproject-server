d0978d027eebd6c017996a3a61c274861b0272c1
Raymond Wangsaatmadja <raykur9@gmail.com>
2022-06-21 14:42:59 +0700

    setup

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,5 @@
 const { User, MyDigimon } = require("../models");
 
-class Controller {
-  static async indexTes(req, res) {
-    try {
-      console.log("masok controller");
-      res.send("Hello World");
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  }
-}
+class Controller {}
 
 module.exports = Controller;
