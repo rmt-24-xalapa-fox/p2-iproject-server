435da83651f6b697e6014a9d173bcc899ef0574a
Hiiyuuri <awalludin.sairan19@gmail.com>
2022-06-21 15:09:04 +0700

    added fiture add Favoriteanime

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,7 +3,33 @@ const { signToken } = require("../helpers/jwt");
 const {User, FavoriteAnime} = require('../models');
 
 class Controller {
+    static async addFavoriteAnime (req, res){
+        try{
+            const {title, currentEpisode, totalEpisode, imgUrl, animeUrl} = req.body
+            const {id} = req.user
+            const response = await FavoriteAnime.create({
+                title,
+                currentEpisode,
+                totalEpisode,
+                imgUrl,
+                animeUrl,
+                UserId: id
+            })
 
+            res.status(201).json({
+                message: `${response.title} has been added to Favorite`
+            })
+
+        }
+        catch(err){
+            res.status(500).json({
+                message: "Internal Server Error"
+            })
+        }
+    }
+    static async updateFavoriteanime(req, res){
+        
+    }
 }
 
 
