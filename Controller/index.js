c207dd8bbedd6d41728c53d109360bff913751ef
harsenn24 <caesarharsenn@gmail.com>
2022-06-21 17:10:14 +0700

    update login fitur

--- Controller/index.js
+++ Controller/index.js
@@ -7,6 +7,47 @@ const midtransClient = require("midtrans-client");
 const { sendEmail } = require("../helps/sendemail");
 
 class Controller {
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        throw { name: "Email is required" };
+      }
+
+      if (!password) {
+        throw { name: "Password is required" };
+      }
+
+      const findUser = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (!findUser) {
+        throw { name: "Invalid email/password" };
+      }
+
+      const checkPass = verifiedPass(password, findUser.password);
+
+      if (!checkPass) {
+        throw { name: "Invalid email/password" };
+      }
+
+      const payload = {
+        id: findUser.id,
+        email: findUser.email,
+      };
+
+      const token = createToken(payload);
+
+      res.status(200).json({ access_token: token });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+  
   static async top10radio(req, res, next) {
     try {
       let filter = {
