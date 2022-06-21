a7b36f444049dab77132bec5ad477efc6d4cc24a
fikarsuwardi <fikarsuwardi@gmail.com>
2022-06-22 00:30:57 +0800

    create detailmovie

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -24,6 +24,18 @@ class Controller {
         next(error)
     }
   }
+
+  static async movieDetail(req, res, next) {
+    try {
+      const { movieId } = req.body
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKeyTMBD}&language=en-US&append_to_response=credits%2Cvideos%2Cimages`
+      );
+      res.status(200).json(response.data);
+    } catch (error) {
+        next(error)
+    }
+  }
 }
 
 module.exports = Controller;
--- routes/movie.js
+++ routes/movie.js
@@ -6,5 +6,6 @@ const Controller = require("../controllers/movieController")
 
 movieRoutes.get("/popular", Controller.moviePopular)
 movieRoutes.get("/upcoming", Controller.movieUpcoming)
+movieRoutes.get("/detail", Controller.movieDetail)
 
 module.exports = movieRoutes
