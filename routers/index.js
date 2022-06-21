8811bb9fa08cee1aa95c32960900678d372391bb
harsenn24 <caesarharsenn@gmail.com>
2022-06-21 17:30:13 +0700

    finish all

--- routers/index.js
+++ routers/index.js
@@ -3,12 +3,14 @@ const router = express.Router()
 const {Controller} = require("../Controller/index.js")
 const { authentif } = require("../middleware/autentif.js")
 
-
+router.post('/register', Controller.register)
 router.post('/login', Controller.login)
 
 router.use(authentif)
 
 router.get('/tokenpayment', Controller.getTokenPayment)
+router.get('/getsong', Controller.getallSong)
+router.get('/top10radio', Controller.top10radio)
 
 
 
