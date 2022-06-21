bc24396b1f630f11d29a4296ca8c455b5778e0b9
banuprabowo <banuprabowo1996@gmail.com>
2022-06-21 11:39:10 +0700

    feat player

--- controller/player.js
+++ controller/player.js
@@ -1,6 +1,50 @@
-const { Rentalan } = require('../models/index')
+const { Rentalan, Player, Unit } = require('../models/index')
+const { hashPassword, comparePassword } = require('../helpers/bcrypt')
+const { convertToToken } = require('../helpers/jwt')
 
 class PlayerController {
+    static async playerRegistration(req, res, next) {
+        try {
+            let { email, password } = req.body
+            if (!email) {
+                throw { name: "email is requrired" }
+            }
+            if (!password) {
+                throw { name: "password is requrired" }
+            }
+            password = hashPassword(password)
+            const registran = await Player.create({ email, password })
+            res.status(201).json({
+                msg: "success register"
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    static async playerLogin(req, res, next) {
+        try {
+            const { email, password } = req.body
+            const player = await Player.findOne({ where: { email } })
+            if (!player) {
+                throw { name: "email/password invalid" }
+            }
+            const valid = comparePassword(password, player.password)
+            if (!valid) {
+                throw { name: "email/password invalid" }
+            }
+            const payload = { id: player.id }
+            const token = convertToToken(payload)
+            res.status(200).json({
+                email,
+                access_token: token
+            })
+        } catch (err) {
+            console.log(err)
+            next(err)
+        }
+    }
+
     static async readRentalan(req, res, next) {
         try {
             const rentalan = await Rentalan.findAll()
@@ -10,6 +54,23 @@ class PlayerController {
         } catch (err) {
             console.log(err);
             next(err)
+        }
+    }
+
+    static async bookUnit(req, res, next) {
+        try {
+            const { UnitId } = req.params
+            const { status } = req.body
+            const found = await Unit.findByPk(UnitId)
+            if (!found) {
+                throw { name: "Not found Unit" }
+            }
+            await Unit.update({ status: status }, { where: { id: UnitId } })
+            res.status(200).json({
+                msg: `Unit where id ${UnitId} booked`
+            })
+        } catch (err) {
+            console.log(err);
         }
     }
 }
--- routers/player.js
+++ routers/player.js
@@ -8,6 +8,9 @@ router.get('/player', (req, res) => {
     res.send('Hello Players!')
 })
 
+router.post('/player/register', PlayerController.playerRegistration)
+router.post('/player/login', PlayerController.playerLogin)
 router.get('/rentalan', PlayerController.readRentalan)
+router.patch('rentalan/:RentalanId/:UnitId', PlayerController.bookUnit)
 
 module.exports = router
