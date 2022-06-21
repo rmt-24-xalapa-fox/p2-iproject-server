8560d5314663478175f5419f702a8731505aea58
ridhokhalis99 <ridhokholis9a@gmail.com>
2022-06-21 17:38:14 +0700

    add: readAllPalettes, readPaletteById, and addPalette method

--- routers/index.js
+++ routers/index.js
@@ -6,6 +6,9 @@ const errorHandler = require("../middlewares/errorHandler");
 router.post("/register", Controller.register);
 router.post("/login", Controller.login);
 router.use(authentication);
+router.get("/palettes", Controller.readAllPalettes);
+router.post("/palettes/add", Controller.addPalette);
+router.get("/palettes/:colorPaletteId", Controller.readPaletteById);
 router.use(errorHandler);
 
 module.exports = router;
