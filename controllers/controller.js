34191935c384a1f6ac7f02b3921cbf6309873801
menciusm <mencius999@gmail.com>
2022-06-23 06:19:50 +0700

    feat: login -- controller

---
+++ controllers/controller.js
@@ -0,0 +1,74 @@
+"use strict";
+const { User, Post, Inbox } = require();
+
+class Controller {
+  static async createPost(req, res, next) {
+    try {
+      const {
+        requestName,
+        requestSeries,
+        uploadImg,
+        fetchedOfferName,
+        fetchedOfferSeries,
+        fetchedOfferImg,
+      } = req.body;
+
+      const { id } = req.user;
+
+      const response = Post.create({
+        UserId: id,
+        requestName,
+        requestSeries,
+        uploadImg,
+        offerName: fetchedOfferName,
+        offerSeries: fetchedOfferSeries,
+        fetchedImg: fetchedOfferImg,
+      });
+      console.log(response);
+      //   res.status(201).json(response);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const targetEmail = await User.findOne({
+        where: { email: email },
+      });
+
+      if (!targetEmail) {
+        throw { name: "Email/password invalid" };
+      }
+
+      const isPassTrue = comparePass(password, targetEmail.password);
+
+      if (!isPassTrue) {
+        throw { name: "Email/password invalid" };
+      }
+
+      //sending token
+
+      const payload = {
+        email: targetEmail.email,
+        role: targetEmail.role,
+      };
+
+      const token = payloadToToken(payload);
+
+      res.status(200).json({
+        statusCode: 200,
+        data: {
+          access_token: token,
+          role: targetEmail.role,
+          id: targetEmail.id,
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = { Controller };
