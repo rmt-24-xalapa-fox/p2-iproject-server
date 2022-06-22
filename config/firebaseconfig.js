728c075078dc4e6a6e7f9514777c48b626ef02ef
YOGA <YOGA@DESKTOP-TQHRCDL>
2022-06-22 17:10:03 +0700

    add fitur register

--- config/firebaseconfig.js
+++ config/firebaseconfig.js
@@ -1,16 +1,19 @@
-const firebase = require('firebase');
+const firebase = require('firebase-admin');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyC-3oiyQY3k0FdKnbu2-WiNMy0_kR0IvJE",
-    authDomain: "chat-app-yoga.firebaseapp.com",
-    projectId: "chat-app-yoga",
-    storageBucket: "chat-app-yoga.appspot.com",
-    messagingSenderId: "553639271253",
-    appId: "1:553639271253:web:1703ed73b6b9b17002eddc",
-    measurementId: "G-ZBMPX8C1BD"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC-3oiyQY3k0FdKnbu2-WiNMy0_kR0IvJE",
+//     authDomain: "chat-app-yoga.firebaseapp.com",
+//     projectId: "chat-app-yoga",
+//     storageBucket: "chat-app-yoga.appspot.com",
+//     messagingSenderId: "553639271253",
+//     appId: "1:553639271253:web:1703ed73b6b9b17002eddc",
+//     measurementId: "G-ZBMPX8C1BD"
+// };
 
-firebase.initializeApp(firebaseConfig);
+let serviceAccount = require("../serviceAccountFirebase/chat-app-yoga-firebase-adminsdk-c6zfb-061214ae95.json");
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount)
+});
 const db = firebase.firestore();
-const User = db.collection("Users");
-module.exports = {User};
+module.exports = {db};
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,32 @@
-const {User} = require("../config/firebaseconfig");
+const {db} = require("../config/firebaseconfig");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 class Controller {
-    
+    static async register(req, res) {
+        try {
+            let {name, password, status, email, imgProfile} = req.body;
+
+            password = bcrypt.hashSync(password, 8)
+
+            let data = {
+                name, password, status, email, imgProfile
+            }
+
+            await db.collection("Users").add(data);
+            res.status(201).json({
+                message: "User has been created"
+            });
+        } catch (error) {
+            console.log(error);
+            const code = 500;
+            const message = "ISE";
+
+            res.status(code).json({
+                message
+            })
+        }
+    }
 }
 
 module.exports = Controller;
