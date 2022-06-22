3752173ad55bc2441678bceebb85b7e56ce1b4d4
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-06-22 11:01:14 +0700

    fix bug in router

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,17 @@ const Controller = require("../controllers/controller");
 const authentication = require("../middlewares/authentication");
 const errorHandler = require("../middlewares/errorHandler");
 
-router.get("/register", Controller.register);
-router.get("/login", Controller.login);
+router.post("/register", Controller.register);
+router.post("/login", Controller.login);
 router.post("/google-sign", Controller.googleSign);
 router.get("/books", Controller.readBooks);
 router.get("/books/:id", Controller.readOneBook);
 router.get("/categories", Controller.readCategories);
 
 router.use(authentication);
+
+router.get("/shipping/cities", Controller.readCities);
+router.post("/shipping/costs", Controller.addShipping);
 
 router.get("/wishlists", Controller.readWishlists);
 router.post("/wishlists/:BookId", Controller.addWishlists);
@@ -19,9 +22,6 @@ router.delete("/wishlists/:BookId", Controller.removeWishlists);
 router.get("/carts", Controller.readCarts);
 router.post("/carts/:BookId", Controller.addCarts);
 router.delete("/carts/:BookId", Controller.removeCarts);
-
-router.get("/shipping/cities", Controller.readCities);
-router.post("/shipping/costs", Controller.addShipping);
 
 router.post("/pay", Controller.createPayment);
 router.get("/orders", Controller.readOrders);
