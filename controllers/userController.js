7ecb99d954b2431afcd92d1835bfd02aa3f52b66
fikarsuwardi <fikarsuwardi@gmail.com>
2022-06-22 14:09:44 +0800

    succes add nodemailer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,129 +1,157 @@
-"use strict"
+"use strict";
 
-const { compareHashWithPass, createToken } = require("../helpers/helper")
-const { User } = require("../models/index")
-const { OAuth2Client } = require('google-auth-library');
+const { compareHashWithPass, createToken } = require("../helpers/helper");
+const { User } = require("../models/index");
+const { OAuth2Client } = require("google-auth-library");
+const nodemailer = require("nodemailer");
+
+// NODEMAILER
+let transporter = nodemailer.createTransport({
+  host: "smtp-mail.outlook.com",
+  secureConnection: false,
+  port: 587,
+  auth: {
+    user: "movieFikar@outlook.com",
+    pass: "sasukee32",
+  },
+});
 
 class Controller {
-    static async listUser(req, res, next) {
-        try {
-            const findUser = await User.findAll({
-                attributes: {exclude: ["password","createdAt","updatedAt"]},
-                order: [['id','ASC']]
-            });
-            res.status(200).json({
-                statusCode: 200,
-                data: findUser
-            });
-
-        } catch (err) {
-            next(err);
-        }
+  static async listUser(req, res, next) {
+    try {
+      const findUser = await User.findAll({
+        attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+        order: [["id", "ASC"]],
+      });
+      res.status(200).json({
+        statusCode: 200,
+        data: findUser,
+      });
+    } catch (err) {
+      next(err);
     }
+  }
 
-    static async register(req, res, next) {
-        try {
-            const { nickname, email, password } = req.body
-            const newUser = await User.create({
-                nickname,
-                email,
-                password,
-            });
+  static async register(req, res, next) {
+    try {
+      const { nickname, email, password } = req.body;
+      const newUser = await User.create({
+        nickname,
+        email,
+        password,
+      });
 
-            if (!newUser) {
-                throw new Error("REGISTRATION_FAILED")
-            }
+      if (!newUser) {
+        throw new Error("REGISTRATION_FAILED");
+      }
 
-            res.status(201).json({
-                statusCode: 201,
-                message: "Succes created user",
-                data: {
-                    id: newUser.id,
-                    nickname: newUser.username,
-                    email: newUser.email,
-                }
-            })
-        } catch (err) {
-            next(err)
+      let mailOptions = {
+        from: '"movieFikar" <movieFikar@outlook.com>', // sender address
+        to: newUser.email, // list of receivers
+        subject: "Thanks for sign in movieFikar", // Subject line
+        text: "Hello " + newUser.nickname + " Thanks for sign in movieFikar", // plain text body
+        html:
+          "<b> Hello '" + newUser.nickname + "' Thanks for sign in movieFikar. </b> <a>Click </a> <a href='http://localhost:3002/login'>here</a><a > to Login</a>" , // html body
+          
+      };
+  
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error, "ini error nodemailer");
+        } else {
+          console.log("Email is send");
         }
+      });
+
+      res.status(201).json({
+        statusCode: 201,
+        message: "Succes created user",
+        data: {
+          id: newUser.id,
+          nickname: newUser.username,
+          email: newUser.email,
+        },
+      });
+    } catch (err) {
+      next(err);
     }
+  }
 
-    static async login(req, res, next) {
-        try {
-            const { email, password } = req.body
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
 
-            const foundUser = await User.findOne({
-                where: {
-                    email
-                }
-            });
+      const foundUser = await User.findOne({
+        where: {
+          email,
+        },
+      });
 
-            if (!foundUser) {
-                throw new Error("USER_NOT_FOUND")
-            }
+      if (!foundUser) {
+        throw new Error("USER_NOT_FOUND");
+      }
 
-            const correctPassword = compareHashWithPass(password, foundUser.password)
+      const correctPassword = compareHashWithPass(password, foundUser.password);
 
-            if (!correctPassword) {
-                throw new Error("INVALID_PASSWORD")
-            }
+      if (!correctPassword) {
+        throw new Error("INVALID_PASSWORD");
+      }
 
-            //bikin payload dulu
-            const payload = {
-                id: foundUser.id,
-                nickname: foundUser.nickname,
-                email: foundUser.email 
-            }
+      //bikin payload dulu
+      const payload = {
+        id: foundUser.id,
+        nickname: foundUser.nickname,
+        email: foundUser.email,
+      };
 
-            const access_token = createToken(payload);
+      const access_token = createToken(payload);
 
-            res.status(200).json({
-                statusCode: 200,
-                message: "Succes Login",
-                access_token: access_token,
-                payload
-            })
-
-        } catch (err) {
-            next(err)
-        }
+      res.status(200).json({
+        statusCode: 200,
+        message: "Succes Login",
+        access_token: access_token,
+        payload,
+      });
+    } catch (err) {
+      next(err);
     }
+  }
 
-    // static async loginGoogle(req, res, next) {
-    //     try {
-    //         const client = new OAuth2Client(process.env.client_id);
-    //         const ticket = await client.verifyIdToken({
-    //             idToken: req.body.credential,
-    //             audience: process.env.client_id,
-    //         });
-    //         const payload = ticket.getPayload();
-    //         const user = await User.findOrCreate({
-    //             where: { email: payload.email },
-    //             defaults: {
-    //                 nickname: 'userGoogle',
-    //                 password: "123456",
-    //             }
-    //         })
+  // static async loginGoogle(req, res, next) {
+  //     try {
+  //         const client = new OAuth2Client(process.env.client_id);
+  //         const ticket = await client.verifyIdToken({
+  //             idToken: req.body.credential,
+  //             audience: process.env.client_id,
+  //         });
+  //         const payload = ticket.getPayload();
+  //         const user = await User.findOrCreate({
+  //             where: { email: payload.email },
+  //             defaults: {
+  //                 nickname: 'userGoogle',
+  //                 password: "123456",
+  //             }
+  //         })
 
-    //         console.log(user, "ini user");
-    //         const dataUser = {
-    //             id: +user.id,
-    //             email: user.email,
-    //         }
+  //         console.log(user, "ini user");
+  //         const dataUser = {
+  //             id: +user.id,
+  //             email: user.email,
+  //         }
 
-    //         const access_token = createToken(dataUser)
+  //         const access_token = createToken(dataUser)
 
-    //         res.status(200).json({
-    //             statusCode: 200,
-    //             message: "Login Google Succes",
-    //             access_token,
-    //             dataUser
-    //         })
+  //         res.status(200).json({
+  //             statusCode: 200,
+  //             message: "Login Google Succes",
+  //             access_token,
+  //             dataUser
+  //         })
 
-    //     } catch (err) {
-    //         next(err)
-    //     }
-    // }
+  //     } catch (err) {
+  //         next(err)
+  //     }
+  // }
 }
 
-module.exports = Controller 
+module.exports = Controller;
