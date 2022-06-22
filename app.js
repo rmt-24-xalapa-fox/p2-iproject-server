57f504d3f16832231948f5abfdcc8bc8cf089112
Raymond Wangsaatmadja <raykur9@gmail.com>
2022-06-22 12:32:13 +0700

    install dotenv

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 const Controller = require("./controllers/controller");
 const authentication = require("./helpers/authentication");
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,11 +1,12 @@
 var jwt = require("jsonwebtoken");
+const secretKey = process.env.SECRET_KEY;
 
 function signToken(payload) {
-  return jwt.sign(payload, "secretgachamon");
+  return jwt.sign(payload, secretKey);
 }
 
 function verifyToken(token) {
-  return jwt.verify(token, "secretgachamon");
+  return jwt.verify(token, secretKey);
 }
 
 module.exports = { signToken, verifyToken };
