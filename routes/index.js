09008a700572f79651a2812ce03d4d1167cb1ebd
arizal <arizalfikri33gmail.com>
2022-06-22 17:03:29 +0700

    hapus route google sign

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.post("/register", Controller.register);
 router.post("/login", Controller.login);
-router.post("/google-sign", Controller.google_sign);
 
 router.use((err, req, res, next) => {
   handleError(err, req, res, next);
