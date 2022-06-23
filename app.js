c4e5864411696a035554f8f62197fcf39735f502
banuprabowo <banuprabowo1996@gmail.com>
2022-06-23 09:43:59 +0700

    deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const passport = require("passport")
 const FacebookStrategy = require("passport-facebook").Strategy
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
