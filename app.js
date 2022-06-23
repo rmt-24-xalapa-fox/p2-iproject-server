43cf6a0d387276303ea2d4b1d5c5ace309bc4e1d
refnaldy <refnaldyhartono7x7@gmail.com>
2022-06-23 09:34:14 +0700

    deployy

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 const errorHandler = require("./middlewares/errorHandler");
 
-// app.use(cors()); //! CORS HARUS MENJADI MIDDLEWARE PALING AWAL
-app.use(
-  cors({
-    origin: "https://instalite-590a5.web.app",
-    credentials: true,
-  })
-);
+app.use(cors()); //! CORS HARUS MENJADI MIDDLEWARE PALING AWAL
+// app.use(
+//   cors({
+//     origin: "https://instalite-590a5.web.app",
+//     credentials: true,
+//   })
+// );
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
