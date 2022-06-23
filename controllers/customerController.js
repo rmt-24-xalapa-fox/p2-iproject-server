e13be839c3843ea8f870134674ea953b91a180ee
raviharyo <raffiiphone99@gmail.com>
2022-06-23 11:51:39 +0700

    fixing bug

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,9 +1,9 @@
 const { compareHash } = require('../helpers/bcrypt')
-const nodemailer = require("nodemailer");
 const { createToken } = require('../helpers/token')
+const nodemailer = require("nodemailer");
 const { User } = require('../models')
 
-const CLIENT_GOOGLE = process.env.CLIENT_GOOGLE
+
 
 // nodemailerneeds
 let transporter = nodemailer.createTransport({
@@ -31,6 +31,7 @@ class customerController {
                 province,
                 city
             })
+
             let mailOptions = {
                 from: '"Cardbas" <santosobudi410@outlook.com>',
                 to: newUser.email,
@@ -99,65 +100,8 @@ class customerController {
         }
     }
 
-    static async googleSignCustomer(req, res, next) {
-        try {
-            const client = new OAuth2Client(CLIENT_GOOGLE);
-            const ticket = await client.verifyIdToken({
-                idToken: req.body.credential,
-                audience: CLIENT_GOOGLE,
-            });
-            const payload = ticket.getPayload();
-
-            let email = payload.email;
-            let customer = await Customer.findOne({
-                where: {
-                    email,
-                },
-            });
-            if (customer) {
-                const payloadd = {
-                    id: customer.id,
-                    email: customer.email,
-
-                }
-                let access_token = createToken({
-                    payloadd
-                });
-                res.status(200).json({
-                    statusCode: 200,
-                    message: `Login dengan google Success`,
-                    access_token: access_token,
-                    data_customer: payloadd
-                });
-            } else {
-                let username = payload.name.split(" ").join("_");
-                let customer = {
-                    username,
-                    email,
-                    password: Math.random().toString(36).substring(1, 7),
-
-                };
-                customer = await Customer.create(customer, { hooks: false })
-                const payloadd = {
-                    id: customer.id,
-                    email: customer.email,
-
-                }
-                let access_token = createToken(payloadd)
-                res.status(201).json({
-                    statusCode: 200,
-                    message: `Register dengan google Success`,
-                    access_token: access_token,
-                    data_customer: payloadd
-                })
-            }
-        } catch (error) {
-            next(error);
-        }
-
-    }
-
 
 }
 
 module.exports = customerController
+
