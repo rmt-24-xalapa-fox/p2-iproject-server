00339fcf8d8d6203300ed5ffb1198ed4825cfca7
menciusm <mencius999@gmail.com>
2022-06-23 08:47:27 +0700

    feat: register routes

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,7 @@
 const { Controller } = require("../controllers/controller");
 const routes = require("express").Router();
 
+routes.post("/register", Controller.register);
 routes.post("/login", Controller.login);
 routes.post("/post", Controller.createPost);
 
