6005731e5a0d922ee5e19e75b054f4de9313538e
banuprabowo <banuprabowo1996@gmail.com>
2022-06-23 09:10:17 +0700

    blm login fb

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const cors = require('cors')
+const passport = require("passport")
+const FacebookStrategy = require("passport-facebook").Strategy
 const app = express()
 const port = 3000
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+// fb login
+passport.use(new FacebookStrategy({
+    clientID: '1176345546521977',
+    clientSecret: 'db3dda8a6f657e33a0a204a579051b0b',
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+app.get('/auth/facebook',
+    passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/secrets');
+    });
 
 const stripe = require("stripe")('sk_test_51LD3zSGsdBMqOjdKPg8LxPZxF4ylvxmDO689Jk0RqsHR2wE0EFh9OCAmbBfvxEJ4kdKeN9xL03xqVzgkVkYzBjQj00pndb8A8E');
 
