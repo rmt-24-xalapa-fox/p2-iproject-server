044b2659ca53ea7a1718934915807e1deeeb187f
CatInBasket <ferdinandus.renaldi@gmail.com>
2022-06-23 17:29:30 +0700

    fix xendit

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 require("dotenv").config();
 
-// const Sequelize = require('sequelize');
-// sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     }
-//   }
-// );
+const Sequelize = require('sequelize');
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 const express = require('express')
 const app = express()
 const cors = require('cors')
